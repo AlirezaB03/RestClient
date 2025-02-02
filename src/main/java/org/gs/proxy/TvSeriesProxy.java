d2c6b33cc3b2f18ed5fb28a433f0a20f3a838e97
amir <amirbahraami99@gmail.com>
2025-02-02 20:46:48 +0330

    fix resteasy

--- src/main/java/org/gs/proxy/TvSeriesProxy.java
+++ src/main/java/org/gs/proxy/TvSeriesProxy.java
@@ -10,7 +10,8 @@ import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import org.gs.TvSerie;
 
 @ApplicationScoped
-@RegisterRestClient(baseUri = "https://api.tvmaze.com/singlesearch")
+@Path("/singlesearch")
+@RegisterRestClient(baseUri = "https://api.tvmaze.com")
 public interface TvSeriesProxy {
     @GET
     @Path("/shows")
